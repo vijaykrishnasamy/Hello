275b10e78a767a08b15be13c22376c410eedfd98
A2FS6ZZ <A2FS6ZZ@usac.mmm.com>
2017-03-25 19:27:48 -0500

    commit for new changes

--- Hello/src/com/test/HelloWorld.java
+++ Hello/src/com/test/HelloWorld.java
@@ -3,8 +3,7 @@ package com.test;
 public class HelloWorld {
 
 	public static void main (String[] args) {
-		System.out.println("Hello World");
-		
+		System.out.println("Hello World");		
 		System.out.println("End");
 	} 
 } 
